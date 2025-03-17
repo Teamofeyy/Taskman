bf37c1b73a1be4b5cf6ca8d7a601ca686491a8e3
Teamofeyy <timarnd06@gmail.com>
2025-03-17 15:50:30 -0400

    bug: broken task status marking

--- bin/taskman.zsh
+++ bin/taskman.zsh
@@ -3,7 +3,7 @@ TASKS_FILE="${HOME}/.taskman_tasks"
 
 case $1 in
   add)
-    echo "[ ] ${@:2}" >> "$TASKS_FILE"
+    echo "[  ${@:2}" >> "$TASKS_FILE"
     echo "Added task: ${@:2}"
     ;;
   list)
