9ea196a7029dc0bd49acf6e1c85911c28ebe09a4
Teamofeyy <timarnd06@gmail.com>
2025-03-17 15:33:55 -0400

    feat: Добавлен функционал удаления задачи

--- bin/taskman.zsh
+++ bin/taskman.zsh
@@ -1,3 +1,11 @@
 #!/bin/bash
 TASKS_FILE="${HOME}/.taskman_tasks"
 
+remove)
+  if [[ "$2" =~ ^[0-9]+$ ]]; then
+    sed -i '' "${2}d" "$TASKS_FILE"
+    echo "Removed task $2"
+  else
+    echo "Invalid task number"
+  fi
+  ;;
