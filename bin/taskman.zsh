c1791abb674daf1100ff41114469b9f77b72b7fc
Teamofeyy <timarnd06@gmail.com>
2025-03-17 15:33:08 -0400

    feat: Добавлена возможность одновлять задачи

--- bin/taskman.zsh
+++ bin/taskman.zsh
@@ -1,3 +1,11 @@
 #!/bin/bash
 TASKS_FILE="${HOME}/.taskman_tasks"
 
+update)
+  if [[ "$2" =~ ^[0-9]+$ ]]; then
+    sed -i '' "${2}s/\[ \]/[x]/" "$TASKS_FILE"
+    echo "Marked task $2 as completed"
+  else
+    echo "Invalid task number"
+  fi
+  ;;
